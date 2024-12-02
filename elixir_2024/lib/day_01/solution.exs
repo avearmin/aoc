e2bdfa55465e56db03fe2ab17ce8a7bb19e08fb2
avearmin <arminradoncic1@gmail.com>
2024-12-01 22:13:37 -0500

    Complete part 1 of day 1

---
+++ elixir_2024/lib/day_01/solution.exs
@@ -0,0 +1,51 @@
+defmodule Day01 do
+  def run(file_path) do
+    file_path
+    |> process_input()
+    |> sort_lists()
+    |> zip_lists_then_diff()
+    |> Enum.sum()
+    |> IO.puts()
+  end
+
+  def process_input(file_path) do
+    case File.read(file_path) do
+      {:ok, content} ->
+        content
+        |> process_content()
+      {:error, message} ->
+        IO.puts(message)
+        {[], []}
+    end 
+  end
+  
+  def process_content(content) do
+    content
+    |> String.split("\n", trim: true)
+    |> Enum.reduce({[], []}, fn line, {first, second} -> 
+      process_line(line, {first, second}) 
+    end)
+ 
+  end
+
+  def process_line(line, {first, second}) do
+    [left, right] = String.split(line, " ", trim: true)
+    {[String.to_integer(left) | first], [String.to_integer(right) | second]}
+  end
+
+  def sort_lists({first, second}) do
+    {Enum.sort(first), Enum.sort(second)} 
+  end
+
+  def zip_lists_then_diff({first, second}) do
+      Enum.zip_reduce(first, second, [], fn left, right, acc -> 
+        diff = Enum.max([left, right]) - Enum.min([left, right])
+        [diff] ++ acc
+    end)
+  end
+
+end
+
+Day01.run("./lib/day_01/input.txt")
+Day01.run("./lib/day_01/test1.txt")
+Day01.run("./lib/day_01/test2.txt")
